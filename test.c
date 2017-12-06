d7ccdfdf335385d38847a7c4fd995b02c93f69c1
Md. Rafsan Jani <rafsanteto@gmail.com>
2017-12-07 02:45:05 +0600

    updated test constant

--- test.c
+++ test.c
@@ -5,7 +5,7 @@ int main()
     char hello[]={"Hello"};
     char *world="World";
 
-    printf("*%d*\n",printf("Hello World %d",66));
+    printf("*%d*\n",printf("Hello World %d",66555455));
     printf("%d %d\n",sizeof(hello),sizeof(world));
     printf("%c %c\n",hello[6],world[6]);
     printf("%d %d\n",hello[6],world[6]);
